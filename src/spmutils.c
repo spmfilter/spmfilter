44c8ae0ddb9083ee922da4996c04fa9411a69235
Axel Steiner <ast@treibsand.com>
2009-08-04 12:33:53 +0000

    added pcre fallback function, if glib2 <= 2.14

--- src/spmutils.c
+++ src/spmutils.c
@@ -1,10 +1,14 @@
 #include <string.h>
 #include <syslog.h>
 #include <glib.h>
+#if (GLIB2_VERSION < 21400)
+#include <pcre.h>
+#endif
 
 #include "spmfilter.h"
 
 char *get_substring(const char *pattern, const char *haystack, int pos) {
+#if (GLIB2_VERSION >= 21400)
 	GRegex *re;
 	GMatchInfo *match_info;
 	char *value = NULL;
@@ -19,6 +23,34 @@ char *get_substring(const char *pattern, const char *haystack, int pos) {
 	g_match_info_free(match_info);
 	g_regex_unref(re);
 	return value;
+#else
+	pcre *re;
+	const char *error;
+	int rc, erroffset, i;
+	int ovector[30];
+	const char *strptr;
+	char *value;
+	char *line;
+
+	line = g_strdup(haystack);
+	re = pcre_compile(pattern, PCRE_CASELESS, &error, &erroffset, NULL);
+	if(re == NULL) {
+		syslog(LOG_NOTICE, "pcre_match : failed to compile pattern %s", pattern);
+	} else {
+		rc = pcre_exec(re, NULL, line, strlen(haystack), 0, 0, ovector, 30);
+		if(rc > 0) {
+			pcre_get_substring(line,ovector,rc,pos,&strptr);
+			value = g_strdup((char *)strptr);
+		} else {
+			syslog(LOG_ERR, "pcre_match : failed to match pattern %s : code was %d", pattern, rc);
+		}
+	}
+	if (strptr != NULL)
+		free((char *) strptr);
+
+	g_free(line);
+	return value;
+#endif
 }
 
 void header_check (gpointer k, gpointer v, gpointer user_data) {
