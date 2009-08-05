b6941a6116115c810b596cbcf696cece6df55bb7
ast <devnull@localhost>
2009-08-05 10:49:29 +0200

    Removed header_checks functions. It's no necessary to configure wanted headers in the config file, instead there will be new functions.

--- src/spmutils.c
+++ src/spmutils.c
@@ -32,14 +32,14 @@ char *get_substring(const char *pattern, const char *haystack, int pos) {
 	char *value;
 	char *line;
 
-	line = g_strdup(haystack);
+//	line = g_strdup(haystack);
 	re = pcre_compile(pattern, PCRE_CASELESS, &error, &erroffset, NULL);
 	if(re == NULL) {
 		syslog(LOG_NOTICE, "pcre_match : failed to compile pattern %s", pattern);
 	} else {
-		rc = pcre_exec(re, NULL, line, strlen(haystack), 0, 0, ovector, 30);
+		rc = pcre_exec(re, NULL, haystack, strlen(haystack), 0, 0, ovector, 30);
 		if(rc > 0) {
-			pcre_get_substring(line,ovector,rc,pos,&strptr);
+			pcre_get_substring(haystack,ovector,rc,pos,&strptr);
 			value = g_strdup((char *)strptr);
 		} else {
 			syslog(LOG_ERR, "pcre_match : failed to match pattern %s : code was %d", pattern, rc);
@@ -48,23 +48,7 @@ char *get_substring(const char *pattern, const char *haystack, int pos) {
 	if (strptr != NULL)
 		free((char *) strptr);
 
-	g_free(line);
+//	g_free(line);
 	return value;
 #endif
-}
-
-void header_check (gpointer k, gpointer v, gpointer user_data) {
-	HL *header_lookup = user_data;
-	gchar *key = k;
-	GList *keys, *l;
-	HEADER *header;
-
-	header = g_hash_table_lookup(header_lookup->settings->header_checks,k);
-	header->value = g_mime_message_get_header(header_lookup->message,header->name);
-	if (header->value != NULL) {
-		if (header_lookup->settings->debug) {
-			syslog(LOG_DEBUG,"found header: %s",header->name);
-			syslog(LOG_DEBUG,"mconn->header_results: added %s:%s", k,header->value);
-		}
-	}
 }
