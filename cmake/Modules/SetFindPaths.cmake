5d06665d63886e77628c44fa2db84d1ec79e2e23
Sebastian Jaekel <sebastian.jaekel@googlemail.com>
2009-12-21 12:15:09 +0100

    - working on cmake integration

---
+++ cmake/Modules/SetFindPaths.cmake
@@ -0,0 +1,20 @@
+#
+# set the search paths for libraries and and includes
+#
+
+set(INCL_DIRS
+	/usr/include
+	/usr/local/include
+	/opt/local/include
+	/opt/gnome/include
+)
+
+set(LIB_DIRS
+	/usr/lib
+	/usr/lib64
+	/usr/local/lib
+	/usr/local/lib64
+	/opt/local/lib
+	/opt/gnome/lib
+)
+
