486ed84dc2daf23f057aacd3228f80eca68393a2
Axel Steiner <ast@space.net>
2010-01-29 17:49:21 +0100

    ldap lookup queries are now returned as LookupResult_T. Still a little bit buggy.

--- src/lookup.h
+++ src/lookup.h
@@ -1,6 +1,8 @@
 #ifndef _LOOKUP_H
 #define	_LOOKUP_H
 
+#include "spmfilter.h"
+
 /** expands placeholders in a user querystring
  *
  * \param format format string to use as input
@@ -11,6 +13,22 @@
  * \returns the number of replacements made or -1 in case of error
  */
 int expand_query(char *format, char *addr, char **buf);
+
+/** Creates a new element for LookupResult_T
+ *
+ * \returns pointer to new allocated LookupRow_T
+ */
+LookupElement_T *lookup_element_new(void);
+
+/** Inserts a new key and value into LookupElement_T
+ *  If the key already exists in LookupElement_T its current value is
+ *  replaced with the new value.
+ *
+ * \param *e pointer to LookupElement_T
+ * \param *key a key to insert
+ * \param *value the value to associate with the key
+ */
+void lookup_element_insert(LookupElement_T *e, char *key, void *value);
 
 #ifdef HAVE_ZDB
 /** Connect to sql server
@@ -46,6 +64,8 @@ void ldap_disconnect(void);
  * \return 1 if the user exists, otherwise 0
  */
 int ldap_user_exists(char *addr);
+
+LookupResult_T *ldap_query(const char *q, ...);
 #endif
 
 #endif	/* _LOOKUP_H */
