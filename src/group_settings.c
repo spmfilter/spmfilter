138a7ea7ff272c8260ea6c80a79e49ef29d7fbe8
Axel Steiner <ast@treibsand.com>
2010-02-04 17:42:52 +0100

    added some documentation for group settings

--- src/group_settings.c
+++ src/group_settings.c
@@ -56,27 +56,69 @@ int group_settings_load(char *group_name) {
 	return 0;
 }
 
+/** Returns the raw value associated with key under the selected group.
+ *  Use group_settings_get_string() to retrieve an unescaped UTF-8 string.
+ *
+ * \param key a key
+ *
+ * \returns a newly allocated string or NULL if the specified key cannot be found.
+ */
+char *group_settings_get_value(char *key) {
+	return g_key_file_get_value(keyfile,group,key,NULL);
+}
+
+/** Returns the string value associated with key under the selected group.
+ *  Unlike group_settings_get_value(), this function handles escape
+ *  sequences like \s.
+ *
+ * \param key a key
+ *
+ * \returns a newly allocated string or NULL if the specified key cannot be found.
+ */
 char *group_settings_get_string(char *key) {
 	return g_key_file_get_string(keyfile, group, key,NULL);
 }
 
+/** Returns the boolean values associated with key under the selected group as an integer.
+ *
+ * \param key a key
+ *
+ * \returns the value associated with the key as a integer, or 0 if the key was
+ * not found or could not be parsed.
+ */
 int group_settings_get_boolean(char *key) {
 	return g_key_file_get_boolean(keyfile, group, key,NULL);
 }
 
+/** Returns the value associated with key under the selected group as an integer.
+ *
+ * \param key a key
+ *
+ * \returns he value associated with the key as an integer, or 0 if the key was not found or could not be parsed.
+ */
 int group_settings_get_integer(char *key) {
 	return g_key_file_get_integer(keyfile, group, key,NULL);
 }
 
+/** Returns the value associated with key under the selected group as a double.
+ *
+ * \param key a key
+ *
+ * \returns the value associated with the key as a double, or 0.0 if the key was not found or could not be parsed.
+ */
 double group_settings_get_double(char *key) {
 	return g_key_file_get_double(keyfile, group, key, NULL);
 }
 
-char **group_settings_get_string_list(char *key, int *length) {
+/** Returns the values associated with key under the selected group.
+ *
+ * \param key a key
+ *
+ * \returns a NULL-terminated string array or NULL if the specified key cannot be found.
+ */
+char **group_settings_get_string_list(char *key, int length) {
 	return g_key_file_get_string_list(keyfile, group, key, &length, NULL);
 }
-
-
 
 /** Free allocated space */
 void group_settings_free(void) {
