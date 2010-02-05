e36ea5847396ee2f6afe98e98dbb9b470f91138d
Sebastian Jaekel <sj@space.net>
2010-02-05 15:12:47 +0100

    - worksaver

--- src/smf_core.h
+++ src/smf_core.h
@@ -9,21 +9,30 @@
 
 #include <glib.h>
 
+#include "spmfilter.h"
+
+typedef int (*ModuleLoadFunction)(MailConn_T *mconn);
+
 struct process_queue_ {
 	GPtrArray *queue;
-	void (*errhandler)(void *args);
+	int (*load_error)(void *args);
+	int (*processing_error)(int retval, void *args);
 };
 
 typedef struct process_queue_  ProcessQueue_T;
 
 
 /** initialize the process queue */
-ProcessQueue_T *smf_core_pqueue_init(void(*errhandler)(void *args));
+ProcessQueue_T *smf_core_pqueue_init(int(*loaderr)(void *args),
+	int (*processerr)(int retval, void *args));
 
 /** loads modules according to configuration and puts them into to
  *  array
  */
 int smf_core_load_modules(ProcessQueue_T *q);
+
+/** run the queue of loaded modules */
+int smf_core_process_modules(ProcessQueue_T *q, MailConn_T *mconn);
 
 #endif	/* _SMF_CORE_H */
 
