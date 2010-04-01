fadb1043a09782c1d24446613edddd3fcdaf9224
Axel Steiner <ast@space.net>
2010-04-01 10:53:58 +0200

    Changed cmake behavoir. Now cmake will check for zdb, ldap and db4 automatically.

--- cmake/Modules/FindDB4.cmake
+++ cmake/Modules/FindDB4.cmake
@@ -2,11 +2,11 @@
 #
 
 find_library(HAVE_DB4 db)
-if(HAVE_DB4-NOTFOUND)
-	message(FATAL_ERROR "db4 has been enabled but library coud not be found")
-else(HAVE_DB4-NOTFOUND)
+if(HAVE_DB4)
 	get_filename_component(DB4_PATH "${HAVE_DB4}" PATH)
 	string(REGEX REPLACE "lib$" "include" DB4_INCLUDE_PATH ${DB4_PATH})
-	message(STATUS "Found db4 library ${HAVE_DB4}")
-	message(STATUS "Found db4 include path ${DB4_INCLUDE_PATH}")
-endif(HAVE_DB4-NOTFOUND)
+	message(STATUS "  found db4 library ${HAVE_DB4}")
+	message(STATUS "  found db4 include path ${DB4_INCLUDE_PATH}")
+else(HAVE_DB4)
+	message(STATUS "  db4 library could not be found, disabling db4 backend.")
+endif(HAVE_DB4)
--- cmake/Modules/FindZdb.cmake
+++ cmake/Modules/FindZdb.cmake
@@ -2,10 +2,8 @@
 #
 
 find_library(HAVE_ZDB zdb)
-if(HAVE_ZDB-NOTFOUND)
-	message(FATAL_ERROR "zdb has been enabled but library coud not be found")
-else(HAVE_ZDB-NOTFOUND)
-	message(STATUS "Found zdb library ${HAVE_ZDB}")
+if(HAVE_ZDB)
+	message(STATUS "  found zdb library ${HAVE_ZDB}")
 	find_path(ZDB_HEADER_DIR NAMES 
 		"zdb/URL.h" 
 		"zdb/ResultSet.h" 
@@ -15,5 +13,7 @@ else(HAVE_ZDB-NOTFOUND)
 		"zdb/SQLException.h" 
 		"zdb/Exception.h")
 	string(REGEX REPLACE "include" "include/zdb" ZDB_INCLUDE_DIR ${ZDB_HEADER_DIR})
-	message(STATUS "Found zdb include path ${ZDB_INCLUDE_DIR}")
-endif(HAVE_ZDB-NOTFOUND)
+	message(STATUS "  found zdb include path ${ZDB_INCLUDE_DIR}")
+else(HAVE_ZDB)
+	message(STATUS "  zdb library could not be found, disabling sql backend.")
+endif(HAVE_ZDB)
