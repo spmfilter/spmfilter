aac5f3475a73dd541f5784189bb551c267ac31a6
Werner Detter <werner@aloah-from-hell.de>
2010-06-01 17:24:05 +0200

    fixed macro

--- src/smf_trace.h
+++ src/smf_trace.h
@@ -44,7 +44,7 @@ void trace(SMFTrace_T level, const char * module, const char * function, int lin
 #define TRDEBUG(fmt, ...) TRACE(TRACE_DEBUG, fmt, ##__VA_ARGS__)
 #define TRINFO(fmt, ...) TRACE(TRACE_INFO, fmt, ##__VA_ARGS__)
 #define TRNOTICE(fmt, ...) TRACE(TRACE_NOTICE, fmt, ##__VA_ARGS__)
-#define TRWARN(fmt, ...) TRACE(TRACE_WARN, fmt, ##__VA_ARGS__)
+#define TRWARN(fmt, ...) TRACE(TRACE_WARNING, fmt, ##__VA_ARGS__)
 #define TRERR(fmt, ...) TRACE(TRACE_ERR, fmt, ##__VA_ARGS__)
 #define TRCRIT(fmt, ...) TRACE(TRACE_CRIT, fmt, ##__VA_ARGS__)
 #define TRALERT(fmt, ...) TRACE(TRACE_ALERT, fmt, ##__VA_ARGS__)
