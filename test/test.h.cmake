29f111c4530562bd480ec7c5d4bcb78158cba388
Axel Steiner <ast@space.net>
2012-07-20 16:12:43 +0200

    added message parsing tests

---
+++ test/test.h.cmake
@@ -0,0 +1,31 @@
+/* spmfilter - mail filtering framework
+ * Copyright (C) 2009-2012 Axel Steiner and SpaceNet AG
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TEST_H
+#define _TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SAMPLES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/test/samples"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _TEST_H */
