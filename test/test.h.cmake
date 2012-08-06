22f1482410d54bd890546a03a75766f4f5ca8826
Axel Steiner <ast@space.net>
2012-08-06 11:11:10 +0200

    finished SMFMessage_T refactoring...for now

--- test/test.h.cmake
+++ test/test.h.cmake
@@ -42,8 +42,6 @@ quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, \
 arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. \
 Nullam dictum felis eu pede mollis pretium.";
 
-char addr_string1[] = "Foo Bar <foobar@example.com>";
-
 char test_name[] = "John Doe";
 char test_email[] = "foo@bar.com";
 char test_addr[] = "John Doe <foo@bar.com>";
