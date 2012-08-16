9004543522f76e19eab208dbe56f1fec333e7bff
Werner Detter <werner@aloah-from-hell.de>
2012-08-16 09:39:28 +0200

    modified comments related to sql

--- src/smf_lookup.h
+++ src/smf_lookup.h
@@ -49,51 +49,53 @@ typedef struct {
 	char **data;
 } SMFLdapValue_T;
 
-/** Frees all of the memory used by SMFLookupResult_T
- *
- * \param *l pointer to SMFLookupResult_T
+/*!
+ * @fn *smf_lookup_result_free (SMFLookupResult_T *l);
+ * @brief Frees all of the memory used by SMFLookupResult_T
+ * @param *l pointer to SMFLookupResult_T list
+ * @returns 
  */
 void smf_lookup_result_free(SMFLookupResult_T *l);
 
-/** Returns SMFLookupElement_T at the given index of SMFLookupResult_T
- *
- * \param l SMFLookupResult_T list
- * \param i the index of the pointer to return
- *
- * \returns SMFLookupElement_T at the given index
+/*!
+ * @fn *smf_lookup_result_index (SMFLookupResult_T *l, int i);
+ * @brief get an SMFLookupElement_T by specified index
+ * @param *l SMFLookupResult_T list
+ * @param i the index of the pointer to return
+ * @returns SMFLookupElement_T at the given index of SMFLookupResult_T
  */
 SMFLookupElement_T *smf_lookup_result_index(SMFLookupResult_T *l, int i);
 
-/** Looks up a key in SMFLookupElement_T
- *
- * \param *e pointer to SMFLookupElement_T
- * \param *key the key to look up
- *
- * \returns the associated value, or NULL if the key is not found
+/*!
+ * @fn *smf_lookup_element_get (SMFLookupElement_T *e, char *key);
+ * @brief Looks up a key in SMFLookupElement_T
+ * @param *e pointer to SMFLookupElement_T
+ * @param *key the key to look up
+ * @returns the associated value, or NULL if the key is not found
  */
 void *smf_lookup_element_get(SMFLookupElement_T *e, char *key);
 
-/** Query lookup backend.
- *
- * \param q a standard printf() format string
- * \param args the list of parameters to insert into the format string
- *
- * \return new allocated SMFLookupResult_T
+/*!
+ * @fn *smf_lookup_query (const char *q, ...);
+ * @brief Query lookup backend
+ * @param q a standard printf() format string
+ * @returns new allocated SMFLookupResult_T
  */
 SMFLookupResult_T *smf_lookup_query(const char *q, ...);
 
-/** Establish database/ldap connection
- *
- * \returns 0 on success or -1 in case of error
+/*!
+ * @fn *smf_lookup_connect (void);
+ * @brief Establish database/ldap connection
+ * @returns 0 on success or -1 in case of error
  */
 int smf_lookup_connect(void);
 
-/** Destroy database/ldap connection
- *
- * \returns 0 on success or -1 in case of error
+/*!
+ * @fn *smf_lookup_disconnect (void);
+ * @brief destroy database/ldap connection 
+ * @returns 0 on success or -1 in case of error
  */
 int smf_lookup_disconnect(void);
-
 
 /*!
  * @fn *smf_lookup_db4_query (char *database, char *key);
@@ -111,27 +113,19 @@ char *smf_lookup_db4_query(char *database, char *key);
  */
 int smf_lookup_sql_connect(SMFSettings_T *settings);
 
-
 /*!
- * @fn *smpf_lookup_sql_disconnect (SMFSettings_T *settings);
- * @brief connect to sql server
- * @param settings Pointer to SMFSettings_T
+ * @fn *smpf_lookup_sql_disconnect (void);
+ * @brief disconnect to sql server
  */
  void smf_lookup_sql_disconnect(void);
 
-/** Query SQL server with given query string
- *
- * \param q format string for sql query
- * \param ... format string arguments
- *
- * \returns SMFLookupResult_T
+/*!
+ * @fn *smf_lookup_sql_query (const char *q, ...);
+ * @brief Query SQL server with given query string
+ * @param q format string for sql query for sql query
+ * @returns SMFLookupResult_T
  */
 SMFLookupResult_T *smf_lookup_sql_query(const char *q, ...);
-
-/** Connect to ldap server
- *
- * \returns 0 on success or -1 in case of error
- */
 
 /*!
  * @fn int smf_lookup_ldap_connect(char *ldap_uri);
@@ -141,7 +135,6 @@ SMFLookupResult_T *smf_lookup_sql_query(const char *q, ...);
  */
 int smf_lookup_ldap_connect(SMFSettings_T *settings);
 
-
 /*!
  * @fn void smf_lookup_ldap_disconnect(char *ldap_uri, SMFSettings_T *settings)
  * @brief disconnect from LDAP Server
@@ -149,7 +142,6 @@ int smf_lookup_ldap_connect(SMFSettings_T *settings);
  * @returns newly allocated char pointer value string for given key, NULL if none found
  */
 void smf_lookup_ldap_disconnect(SMFSettings_T *settings);
-
 
 /*!
  * @fn SMFLookupResult_T *smf_lookup_ldap_query(SMFSettings_T *settings, const char *q, ...)
