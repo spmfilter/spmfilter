5fd419c9ab904ce65313677f2fbef22c47ec94fb
Axel Steiner <ast@space.net>
2012-10-02 14:58:23 +0200

    smtp refactoring

--- src/smf_smtp.c
+++ src/smf_smtp.c
@@ -15,39 +15,197 @@
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <glib.h>
-#include <glib/gstdio.h>
-#include <gmime/gmime.h>
-
+#include <assert.h>
+#include <stdarg.h>
 #include <openssl/ssl.h>
 #include <auth-client.h>
 #include <signal.h>
 #include <libesmtp.h>
+#include <errno.h>
 
 #include "smf_core.h"
 #include "smf_trace.h"
 #include "smf_message.h"
+#include "smf_envelope.h"
 #include "smf_settings.h"
+#include "smf_list.h"
+#include "smf_smtp.h"
 
+/*
 static int authinteract (auth_client_request_t request, char **result, int fields, void *arg);
 static int tlsinteract (char *buf, int buflen, int rwflag, void *arg);
 void event_cb (smtp_session_t session, int event_no, void *arg, ...);
 int handle_invalid_peer_certificate(long vfy_result);
 void print_recipient_status (smtp_recipient_t recipient, const char *mailbox, void *arg);
-
+*/
 #define THIS_MODULE "smtp"
 
-/** Deliver message
- *
- * \param msg_data Message_T structure
- *
- * \returns 0 on success or -1 in case of error
- */
+SMFSmtpStatus_T *smf_smtp_status_new(void) {
+	SMFSmtpStatus_T *status = NULL;
 
-// SMFMessageEnvelope_T *msg_data ersetzen durch SMFSession_T *session
+    status = (SMFSmtpStatus_T *)calloc((size_t)1, sizeof(SMFSmtpStatus_T));
+
+    return status;
+}
+
+void smf_smtp_status_free(SMFSmtpStatus_T *status) {
+	assert(status);
+	if (status->text != NULL) free(status->text);
+	free(status);
+}
+
+SMFSmtpStatus_T *smf_smtp_deliver(SMFEnvelope_T *env, SMFTlsOption_T tls, char *auth_user, char *auth_pass, char *msg_file) {
+	smtp_session_t session;
+	smtp_message_t message;
+	smtp_recipient_t recipient;
+	auth_context_t authctx = NULL;
+	struct sigaction sa;
+	const smtp_status_t *retstat;
+	SMFListElem_T *elem = NULL;
+	char *nexthop = NULL;
+	char *reverse_path = NULL;
+	char *msg_string = NULL;
+	FILE *fp = NULL;
+	char *s = NULL;
+	SMFSmtpStatus_T *status = smf_smtp_status_new();
+
+	assert(env);
+
+	TRACE(TRACE_DEBUG,"initializing SMTP session");
+
+	status->code = -1;
+
+	auth_client_init();	
+	
+	session = smtp_create_session();
+	message = smtp_add_message(session);
+
+	sa.sa_handler = SIG_IGN;
+	sigemptyset (&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction (SIGPIPE, &sa, NULL);
+
+	if (env->nexthop != NULL) {
+		if (strstr(env->nexthop,":"))
+			smtp_set_server(session, env->nexthop);
+		else {
+			asprintf(&nexthop,"%s:25",env->nexthop);
+			smtp_set_server(session,nexthop);
+		}
+	} else {
+		smtp_destroy_session(session);
+		status->code = -1;
+		asprintf(&status->text,"invalid smtp host");
+		TRACE(TRACE_ERR,status->text);
+		return status;
+	}
+
+/*
+	smtp_starttls_enable(session,tls);
+	smtp_starttls_set_password_cb(tlsinteract, NULL);
+	smtp_set_eventcb(session, event_cb, NULL);
+*/
+/*
+	if ((auth_user != NULL) && (auth_pass != NULL)) {
+		authctx = auth_create_context();
+		auth_set_mechanism_flags(authctx, AUTH_PLUGIN_PLAIN, 0);
+		auth_set_interact_cb(authctx, authinteract, msg_data);
+		smtp_auth_set_context (session, authctx);
+	}
+*/
+
+	if(env->sender != NULL)  {
+		reverse_path = strdup(env->sender);
+	} else {
+		/* bounce sender */
+		asprintf(&reverse_path,"<>");
+	}
+
+	if (smtp_set_reverse_path(message,reverse_path) == 0) {
+		asprintf(&status->text,"failed to set reverse_path");
+		status->code = -1;
+		TRACE(TRACE_ERR,status->text);
+		return status;
+	}
+
+	if (msg_file != NULL) {
+		if((fp = fopen(msg_file, "r"))==NULL) {
+        	asprintf(&status->text,"unable to open file: %s (%d)",strerror(errno), errno);
+        	status->code = -1;
+        	TRACE(TRACE_ERR,status->text);
+        	smtp_destroy_session(session);
+        	return status;
+    	}
+    	smtp_set_message_fp(message, fp);
+    } else {
+		if (env->message != NULL) {
+			msg_string = smf_message_to_string(env->message);
+			if (smtp_set_message_str(message,msg_string)==0) {
+				asprintf(&status->text,"failed to create message object");
+				status->code = -1;
+				TRACE(TRACE_ERR,status->text);
+				return status;
+			}
+		} else {
+			asprintf(&status->text,"no message content provided");
+			status->code = -1;
+			TRACE(TRACE_ERR,status->text);
+			smtp_destroy_session(session);
+			return status;
+		}
+	} 
+	
+
+	if (env->recipients->size == 0) {
+		asprintf(&status->text,"no recipients provided");
+		status->code = -1;
+		TRACE(TRACE_ERR,status->text);
+		smtp_destroy_session(session);
+		if (fp != NULL) fclose(fp);
+		return status;
+	}
+
+	elem = smf_list_head(env->recipients);
+	while(elem != NULL) {
+		s = (char *)smf_list_data(elem);
+		recipient = smtp_add_recipient(message,s);
+		elem = elem->next;
+	}
+
+	if (!smtp_start_session(session)) {
+		asprintf(&status->text,"failed to initialize smtp session");
+		status->code = -1;
+		TRACE(TRACE_ERR,status->text);
+		smtp_destroy_session(session);
+		if (fp != NULL) fclose(fp);
+		return status;
+	} else {
+		retstat = smtp_message_transfer_status(message);
+		//smtp_enumerate_recipients(message, print_recipient_status, NULL);
+		status->text = strdup(retstat->text);
+		status->code = retstat->code;
+		TRACE(TRACE_DEBUG,"smtp client got status '%d - %s'",status->code,status->text);
+	}
+
+	smtp_destroy_session(session);
+
+	if (nexthop != NULL) free(nexthop);
+	if (fp != NULL) fclose(fp);
+	if (msg_string != NULL) free(msg_string);
+	if (reverse_path != NULL) free(reverse_path);
+	if (authctx != NULL) {
+		auth_destroy_context(authctx);
+		auth_client_exit();
+	}
+
+	return status;
+}
+
 
 #if 0
 int smf_message_deliver(SMFMessageEnvelope_T *msg_data) {
@@ -175,11 +333,10 @@ int smf_message_deliver(SMFMessageEnvelope_T *msg_data) {
 
 	return ret;
 }
-#endif
 
 static int authinteract (auth_client_request_t request, char **result, int fields, void *arg) {
 	int i;
-	
+
 	SMFMessageEnvelope_T *msg_data = (SMFMessageEnvelope_T *)arg;
 	for (i = 0; i < fields; i++) {
 		if (request[i].flags & AUTH_USER)
@@ -193,9 +350,11 @@ static int authinteract (auth_client_request_t request, char **result, int field
   return 1;
 } 
 
+
 static int tlsinteract(char *buf, int buflen, int rwflag, void *arg) {
 	char *pw;
 	int len;
+
 	SMFSettings_T *settings = smf_settings_get();
 
 	if (settings->tls_pass) {
@@ -206,6 +365,7 @@ static int tlsinteract(char *buf, int buflen, int rwflag, void *arg) {
 		strcpy (buf, pw);
 		return len;
 	} else
+
 		return 0;
 }
 
@@ -326,6 +486,8 @@ int handle_invalid_peer_certificate(long vfy_result) {
 void print_recipient_status (smtp_recipient_t recipient, const char *mailbox, void *arg) {
 	const smtp_status_t *status;
 
-	status = smtp_recipient_status (recipient);
+	status = smtp_recipient_status(recipient);
 	TRACE(TRACE_DEBUG,"%s: %d %s", mailbox, status->code, status->text);
 }
+#endif
+
