4b1677f8015d6d4b3aa32a6b77b22142942b750a
Axel Steiner <ast@space.net>
2012-11-05 14:47:24 +0100

    finished (probably) module tests

--- test/test_modules.c
+++ test/test_modules.c
@@ -15,6 +15,8 @@
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 
 #include "../src/smf_settings.h"
@@ -46,25 +48,47 @@ int main(int argc, char const *argv[]) {
     SMFSettings_T *settings = smf_settings_new();
     SMFProcessQueue_T *q;
     SMFSession_T *session = smf_session_new();
-    SMFEnvelope_T *envelope = smf_envelope_new();
     SMFMessage_T *message = smf_message_new();
-    int ret;
+    SMFEnvelope_T *env = smf_session_get_envelope(session);
     char *s;
 
+    smf_settings_set_debug(settings, 1);
+    smf_envelope_set_nexthop(env, "localhost:25");
+    smf_envelope_set_sender(env, test_email);
+    smf_envelope_add_rcpt(env, test_email);
+
     printf("Start smf_modules tests...\n");
+    printf("============================================\n");
+    printf("This test expects a running SMTP daemon, \n");
+    printf("listening on localhost:25, wich accepts \n");
+    printf("mails for user@example.org\n");
+    printf("============================================\n");
 
-    envelope->message = message;
-    session->envelope = envelope;
+    /* preparing session stuff */
+    asprintf(&s, "%s/m0001.txt",SAMPLES_DIR);
+    smf_message_from_file(&message, s, 0);
+    free(s);
+    session->envelope->message = message;
 
+    smf_settings_set_queue_dir(settings, BINARY_DIR);
+    smf_core_gen_queue_file(settings->queue_dir, &session->message_file, session->id);
     s = smf_message_generate_message_id();
     smf_message_set_message_id(message, s);
     free(s);
+    smf_message_to_file(message, session->message_file);
 
+    /* add test modules */
     smf_settings_add_module(settings, "testmod1");
     smf_settings_add_module(settings, "testmod2");
 
-    smf_modules_init(settings,BINARY_DIR);
+    printf("* testing smf_modules_init()...\t\t\t");
+    if (smf_modules_init(settings,BINARY_DIR)!=0) {
+        printf("failed\n");
+        return -1;
+    }
+    printf("passed\n");
 
+    printf("* testing smf_modules_pqueue_init()...\t\t");
     q = smf_modules_pqueue_init(
         _handle_q_error,
         _handle_q_processing_error,
@@ -72,13 +96,35 @@ int main(int argc, char const *argv[]) {
     );
 
     if(q == NULL) {
+        printf("failed\n");
         return(-1);
     }
+    printf("passed\n");
 
-    ret = smf_modules_process(q,session,settings);
+    printf("* testing smf_modules_process()...\t\t");
+    if (smf_modules_process(q,session,settings)!=0) {
+        printf("failed\n");
+        return -1;
+    }
+    printf("passed\n");
+    
+    printf("* testing smf_modules_deliver_nexthop()...\t");
+    if (smf_modules_deliver_nexthop(settings,q,session)!=0) {
+        printf("failed\n");
+        return -1;
+    }
+    printf("passed\n");
+
+
     free(q);
+    smf_session_free(session);
 
-    smf_modules_unload(settings);
+    printf("* testing smf_modules_unload()...\t\t");
+    if (smf_modules_unload(settings)!=0) {
+        printf("failed\n");
+        return -1;
+    }
+    printf("passed\n");
 
     smf_settings_free(settings);
 
