df9b82ae27d19381413cb2f642f05eba53df88ad
Axel Steiner <ast@space.net>
2012-11-05 14:47:24 +0100

    finished (probably) module tests

--- src/smf_smtpd.c
+++ src/smf_smtpd.c
@@ -1,5 +1,5 @@
 /* spmfilter - mail filtering framework
- * Copyright (C) 2009-2010 Axel Steiner and SpaceNet AG
+ * Copyright (C) 2009-2012 Axel Steiner and SpaceNet AG
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
