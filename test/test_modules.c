faf9d28ccb7ed1aa69f622246a52a111fef80d99
Robin Doer <rd@space.net>
2012-11-13 23:51:25 +0100

    Test for successful run of smf_modules_process()

--- test/test_modules.c
+++ test/test_modules.c
@@ -23,34 +23,130 @@
 #include "../src/smf_modules.h"
 #include "../src/smf_session.h"
 #include "../src/smf_session_private.h"
+#include "../src/smf_settings.h"
+#include "../src/smf_settings_private.h"
 
+#include "test.h"
+
+struct cb_data {
+    int count;
+    int rc;  
+};
+
+static SMFSettings_T *settings;
 static SMFSession_T *session;
-static int mod1_count;
+static SMFProcessQueue_T *queue;
+static struct cb_data mod1_data;
+static struct cb_data mod2_data;
+static struct cb_data mod3_data;
+static struct cb_data error_data;
+static struct cb_data processing_error_data;
+static struct cb_data nexthop_error_data;
+
+static void init_cb_data(struct cb_data* data) {
+    data->count = 0;
+    data->rc = 0;
+}
+
+static SMFMessage_T *load_sample_message(const char *sample) {
+    SMFMessage_T *message;
+    
+    fail_unless((message = smf_message_new()) != NULL);
+    fail_unless(smf_message_from_file(&message, sample, 1) == 0);
+    
+    return message;
+}
 
 static int mod1(SMFSession_T *s) {
     fail_unless(session == s);
-    mod1_count++;
-    return 0;
+    mod1_data.count++;
+    return mod1_data.rc;
+}
+
+static int mod2(SMFSession_T *s) {
+    fail_unless(session == s);
+    mod2_data.count++;
+    return mod2_data.rc;
+}
+
+static int mod3(SMFSession_T *s) {
+    fail_unless(session == s);
+    mod3_data.count++;
+    return mod3_data.rc;
+}
+
+static int _handle_q_error(SMFSettings_T *set, SMFSession_T *ses) {
+    fail_unless(settings == set);
+    fail_unless(session == ses);
+    error_data.count++;
+    return error_data.rc;
+}
+
+static int _handle_q_processing_error(SMFSettings_T *set, SMFSession_T *ses, int retval) {
+    fail_unless(settings == set);
+    fail_unless(session == ses);
+    processing_error_data.count++;
+    return processing_error_data.rc;
+}
+
+static int _handle_nexthop_error(SMFSettings_T *set, SMFSession_T *ses) {
+    fail_unless(settings == set);
+    fail_unless(session == ses);
+    nexthop_error_data.count++;
+    return nexthop_error_data.rc;
 }
 
 static void setup() {
+    fail_unless((settings = smf_settings_new()) != NULL);
+    smf_settings_set_queue_dir(settings, BINARY_DIR);
+        
     fail_unless((session = smf_session_new()) != NULL);
-    mod1_count = 0;
+    
+    queue = smf_modules_pqueue_init(_handle_q_error, _handle_q_processing_error, _handle_nexthop_error);
+    fail_unless(queue != NULL);
+        
+    init_cb_data(&mod1_data);
+    init_cb_data(&mod2_data);
+    init_cb_data(&mod3_data);
+    init_cb_data(&error_data);
+    init_cb_data(&processing_error_data);
+    init_cb_data(&nexthop_error_data);
 }
 
 static void teardown() {
+    smf_settings_free(settings);
     smf_session_free(session);
+    free(queue);
     session = NULL;
 }
 
 START_TEST(create_invoke_destroy_callback) {
     SMFModule_T *module;
     
-    fail_unless(mod1_count == 0);
+    fail_unless(mod1_data.count == 0);
     fail_unless((module = smf_module_create_callback("foo", mod1)) != NULL);
     fail_unless(smf_module_invoke(module, session) == 0);
     fail_unless(smf_module_destroy(module) == 0);
-    fail_unless(mod1_count == 1);
+    fail_unless(mod1_data.count == 1);
+}
+END_TEST
+
+START_TEST(process_success) {
+    SMFMessage_T *message = load_sample_message(SAMPLES_DIR "/m0001.txt");
+        
+    smf_list_append(settings->modules, smf_module_create_callback("mod1", mod1));
+    smf_list_append(settings->modules, smf_module_create_callback("mod2", mod2));
+    smf_list_append(settings->modules, smf_module_create_callback("mod3", mod3));
+    fail_unless(smf_list_size(settings->modules) == 3);
+    
+    session->envelope->message = message;
+    fail_unless(smf_modules_process(queue, session, settings) == 0);
+    fail_unless(mod1_data.count == 1);
+    fail_unless(mod2_data.count == 1);
+    fail_unless(mod3_data.count == 1);
+    fail_unless(error_data.count == 0);
+    fail_unless(processing_error_data.count == 0);
+    fail_unless(nexthop_error_data.count == 0);
 }
 END_TEST
 
@@ -59,6 +155,7 @@ TCase *modules_tcase() {
     tcase_add_checked_fixture(tc, setup, teardown);
     
     tcase_add_test(tc, create_invoke_destroy_callback);
+    tcase_add_test(tc, process_success);
     
     return tc;
 }
