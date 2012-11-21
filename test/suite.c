22ec2ed638679a99b37edadbaccb428f32f15a87
Axel Steiner <ast@space.net>
2012-11-21 16:30:47 +0100

    - new unit tests for smf_envelope - fixed free() bug in test_core

--- test/suite.c
+++ test/suite.c
@@ -21,6 +21,7 @@
 extern TCase *core_tcase();
 extern TCase *modules_tcase();
 extern TCase *ea_tcase();
+extern TCase *env_tcase();
 
 static Suite *smf_suite() {
 	Suite* s = suite_create("smf unit tests");
@@ -28,6 +29,7 @@ static Suite *smf_suite() {
 	suite_add_tcase(s, core_tcase());
 	suite_add_tcase(s, modules_tcase());
     suite_add_tcase(s, ea_tcase());
+    suite_add_tcase(s, env_tcase());
 
 	return s;
 }
