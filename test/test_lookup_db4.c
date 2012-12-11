6ef108074f257a05e0bb954abe599b1927ef686d
Robin Doer <rd@space.net>
2012-12-11 15:10:40 +0100

    smf_lookup_db4_update()
    The function is able to create/update a Berkeley database.

--- test/test_lookup_db4.c
+++ test/test_lookup_db4.c
@@ -17,13 +17,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <db.h>
 #include "../src/smf_lookup.h"
 
 #define TESTDB "/tmp/smf_test_db4.db"
 
 int remove_db(void) {
-    if((remove(TESTDB)) < 0) {
+    if((remove(TESTDB)) < 0 && errno != ENOENT) {
       fprintf(stderr, "Fehler beim Löschen von %s", TESTDB);
       return(-1);
     }
@@ -31,72 +30,39 @@ int remove_db(void) {
 }
 
 int main (int argc, char const *argv[]) {
-    DB *dbp;
-    DBT key, data;
-    u_int32_t flags;  
-    flags = DB_CREATE;
-    int ret;
-
-    char *value_str = "this is a test";
+    char *value_str_1 = "this is a test";
+    char *value_str_2 = "this is a second test";
     char *key_char = "2323";
     char *res_from_db = NULL;
 
     printf("Start smf_lookup_db4 tests...\n");
+    remove_db();
 
     /* first create a new berkeley database */
-    if ((ret = db_create(&dbp, NULL, 0)) != 0) {
-        printf("db_create: %s\n", db_strerror(ret));
-        return(-1);
-    }
-    /* set page- and cachesize */
-    if ((ret = dbp->set_pagesize(dbp, 1024)) != 0) {
-        printf("db->open: %s\n", db_strerror(ret));
-        return(-1);
-    }
-    if ((ret = dbp->set_cachesize(dbp, 0, 32 * 1024, 0)) != 0) {
-        printf("db->open: %s\n", db_strerror(ret));
-        return(-1);
-    }
-    /* open the database */
-    if((ret = dbp->open(dbp, NULL, TESTDB, NULL, DB_HASH, flags, 0)) != 0) {
-        printf("db->open: %s\n", db_strerror(ret));
-        return(-1);
-    }
-    /* write to database, zero out the DBTs before using them */
-    memset(&key, 0, sizeof(DBT));
-    memset(&data, 0, sizeof(DBT));
-    key.data = key_char;
-    key.size = strlen(key_char)+1;
-    data.data = value_str;
-    data.size = strlen(value_str)+1; 
+    printf("* testing smf_lookup_db4_update() (1)...\t\t\t");
+    assert(smf_lookup_db4_update(TESTDB, key_char, value_str_1) == 0);
+    printf("passed\n");
 
-    if((ret = dbp->put(dbp, NULL, &key, &data, 0)) != 0) {
-        printf("db->put: %s\n", db_strerror(ret));
-        return(-1);
-    }
-    /* close the database */
-    if (dbp != NULL)
-        dbp->close(dbp, 0); 
-
-    printf("* testing smf_lookup_db4_query()...\t\t\t");
     /* read out database with spmfilter function and compare the values */ 
-    res_from_db = smf_lookup_db4_query(TESTDB, key_char);
-    if(res_from_db != NULL) {
-        if(strcmp(res_from_db,value_str) != 0) {
-            remove_db();
-            free(res_from_db);
-            printf("failed\n");
-            return(-1);
-        }
-    } else {
-        remove_db();
-        free(res_from_db);
-        printf("failed\n");
-        return(-1);
-    }
-    remove_db();
+    printf("* testing smf_lookup_db4_query() (1)...\t\t\t");
+    assert((res_from_db = smf_lookup_db4_query(TESTDB, key_char)) != NULL);
+    assert(strcmp(res_from_db,value_str_1) == 0);
     free(res_from_db);
     printf("passed\n");
+    
+    /* Update an already existing key */
+    printf("* testing smf_lookup_db4_update() (2)...\t\t\t");
+    assert(smf_lookup_db4_update(TESTDB, key_char, value_str_2) == 0);
+    printf("passed\n");
+
+    /* read out database again */ 
+    printf("* testing smf_lookup_db4_query() (2)...\t\t\t");
+    assert((res_from_db = smf_lookup_db4_query(TESTDB, key_char)) != NULL);
+    assert(strcmp(res_from_db,value_str_2) == 0);
+    free(res_from_db);
+    printf("passed\n");
+
+    remove_db();
 
     return(0);
 }
