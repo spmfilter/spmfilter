1f52745d74c529b8ba16c9a2690f88c3a39c3864
Axel Steiner <ast@treibsand.com>
2014-01-16 09:56:44 +0100

    Added PREFIX/include to pkg-config cflags for cmime dependency.

--- src/spmfilter.pc.cmake
+++ src/spmfilter.pc.cmake
@@ -1,11 +1,12 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@CMAKE_INSTALL_FULL_LIBDIR@/spmfilter
-includedir=${prefix}/include/spmfilter
+includedir=${prefix}/include
+smf_includedir=${includedir}/spmfilter
 
 Name: spmfilter
 Version: @SMF_VERSION@
 Description: Core libraries of spmfilter
 URL: http://www.space.net
 Libs: -L${libdir} -lsmf
-Cflags: -I${includedir}
+Cflags: -I${includedir} -I${smf_includedir}
