ade05c8195a357ade63f1b5ff1a4235fdeb21278
Axel Steiner <ast@treibsand.com>
2019-02-14 10:32:22 +0100

    Added testing spmfilter.conf.cmake and cmake configuration for test environment

---
+++ test/samples/spmfilter.conf.cmake
@@ -0,0 +1,17 @@
+[global]
+engine = smtpd
+lib_dir = ${CMAKE_CURRENT_BINARY_DIR}/test
+foreground = true
+add_header = false
+queue_dir = ${CMAKE_CURRENT_BINARY_DIR}/test
+bind_ip = 127.0.0.1
+bind_port = 12525
+nexthop = ${TEST_NEXTHOP}
+spare_childs = 2
+max_childs = 10
+pid_file = ${CMAKE_CURRENT_BINARY_DIR}/test/smf_test_smtpd.pid
+user = vpopmail
+group = vchkpw
+modules = testmod1, testmod2
+module_fail = 3
+debug = true
